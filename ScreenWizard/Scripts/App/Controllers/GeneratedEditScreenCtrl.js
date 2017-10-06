6fc46a159bacdeabdb762daaaa0e6fc4cb30e534
BenHargreaves <bhargreaves91@gmail.com>
2017-10-06 01:10:32 -0400

    Added functionality to save invoice line items to an Invoice. Hid 'List' type items from existing list screens

--- ScreenWizard/Scripts/App/Controllers/GeneratedEditScreenCtrl.js
+++ ScreenWizard/Scripts/App/Controllers/GeneratedEditScreenCtrl.js
@@ -17,17 +17,34 @@
                 $scope.screenSubmit.$setPristine();
             };
 
+            $scope.Items = [];
+
+            $scope.addRow = function () {
+                var row = {};
+                for (var j = 0; j < $scope.screenData.Fields.length; j++) {
+                    if ($scope.screenData.Fields[j].Type == 'List') {
+                        for (var i = 0; i < $scope.screenData.Fields[j].ItemDetails.length; i++) {
+                            var name = $scope.screenData.Fields[j].ItemDetails[i].Name;
+                            var value = $scope.screenData.Fields[j].ItemDetails[i].Value;
+                            row[name] = value;
+                        };
+                        $scope.Items.push(row);
+                    };
+                };
+            };
+
             $scope.submit = function () {
                 var data = {};
                 for (var i = 0; i < $scope.screenData.Fields.length; i++) {
-                    if ($scope.screenData.Fields[i].Name != 'List') {
+                    if ($scope.screenData.Fields[i].Type != 'List') {
                         var name = $scope.screenData.Fields[i].Name;
                         var value = $scope.screenData.Fields[i].Value;
                         data[name] = value;
                     }
                     else {
-
-                    }
+                        var name = $scope.screenData.Fields[i].Name;
+                        data[name] = $scope.Items;
+                    };
                 };
                 $http.put(urlBase + $scope.screenData.TableName + "/create", data)
                     .then (function(response){
